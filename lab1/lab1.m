d3c3575efc5161bd932ca847745b0c9d92aa8341
Jorge López <jorge.lopez@neosentec.com>
2018-12-17 18:28:18 +0100

    Simplified lines transformations.

--- lab1/lab1.m
+++ lab1/lab1.m
@@ -81,7 +81,6 @@ end
 %% 1.3 Projective transformations (homographies)
 
 % ToDo: generate a matrix H which produces a projective transformation
-close all
 H2 = H1;
 H2(3,1:2) = [0,0.0005];
 I2 = apply_H(I, H2);
@@ -89,6 +88,7 @@ figure; imshow(I); figure; imshow(uint8(I2));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% 2. Affine Rectification
+close all
 
 % choose the image points
 I=imread('Data/0000_s.png');
@@ -111,16 +111,16 @@ p8 = [A(i,3) A(i,4) 1]';
 % ToDo: compute the lines l1, l2, l3, l4, that pass through the different pairs of points
 
 coefficients = polyfit([p1(1), p2(1)], [p1(2), p2(2)], 1);
-l1 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
+l1 = [coefficients(1) -1 coefficients(2)];
 
 coefficients = polyfit([p3(1), p4(1)], [p3(2), p4(2)], 1);
-l2 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
+l2 = [coefficients(1) -1 coefficients(2)];
 
 coefficients = polyfit([p5(1), p6(1)], [p5(2), p6(2)], 1);
-l3 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
+l3 = [coefficients(1) -1 coefficients(2)];
 
 coefficients = polyfit([p7(1), p8(1)], [p7(2), p8(2)], 1);
-l4 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
+l4 = [coefficients(1) -1 coefficients(2)];
 
 % show the chosen lines in the image
 figure;imshow(I);
@@ -145,40 +145,17 @@ vl = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
 Hp = [1 0 0; 0 1 0; vl];
 
 I2 = apply_H(I, Hp);
-figure; imshow(uint8(I2));
+% figure; imshow(uint8(I2));
 
 % ToDo: compute the transformed lines lr1, lr2, lr3, lr4
 
-% First we transform the points
-pr1 = Hp*p1;
-pr1 = [pr1(1)/ pr1(3), pr1(2)/pr1(3), 1];
-pr2 = Hp*p2;
-pr2 = [pr2(1)/ pr2(3), pr2(2)/pr2(3), 1];
-pr3 = Hp*p3;
-pr3 = [pr3(1)/ pr3(3), pr3(2)/pr3(3), 1];
-pr4 = Hp*p4;
-pr4 = [pr4(1)/ pr4(3), pr4(2)/pr4(3), 1];
-pr5 = Hp*p5;
-pr5 = [pr5(1)/ pr5(3), pr5(2)/pr5(3), 1];
-pr6 = Hp*p6;
-pr6 = [pr6(1)/ pr6(3), pr6(2)/pr6(3), 1];
-pr7 = Hp*p7;
-pr7 = [pr7(1)/ pr7(3), pr7(2)/pr7(3), 1];
-pr8 = Hp*p8;
-pr8 = [pr8(1)/ pr8(3), pr8(2)/pr8(3), 1];
+lr1 = inv(Hp)' * l1';
 
-% Now we compute the lines from the transformed points
-coefficients = polyfit([pr1(1), pr2(1)], [pr1(2), pr2(2)], 1);
-lr1 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
+lr2 = inv(Hp)' * l2';
 
-coefficients = polyfit([pr3(1), pr4(1)], [pr3(2), pr4(2)], 1);
-lr2 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
+lr3 = inv(Hp)' * l3';
 
-coefficients = polyfit([pr5(1), pr6(1)], [pr5(2), pr6(2)], 1);
-lr3 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
-
-coefficients = polyfit([pr7(1), pr8(1)], [pr7(2), pr8(2)], 1);
-lr4 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
+lr4 = inv(Hp)' * l4';
 
 % show the transformed lines in the transformed image
 figure;imshow(uint8(I2));
@@ -188,6 +165,7 @@ plot(t, -(lr1(1)*t + lr1(3)) / lr1(2), 'y');
 plot(t, -(lr2(1)*t + lr2(3)) / lr2(2), 'y');
 plot(t, -(lr3(1)*t + lr3(3)) / lr3(2), 'y');
 plot(t, -(lr4(1)*t + lr4(3)) / lr4(2), 'y');
+hold off;
 
 % ToDo: to evaluate the results, compute the angle between the different pair 
 % of lines before and after the image transformation
@@ -217,11 +195,16 @@ disp("Angle 2 rectified:");disp(angler2);
 %       Compute also the angles between the pair of lines before and after
 %       rectification.
 
+lr1 = [lr1(1) / lr1(3), lr1(2)/lr1(3), 1];
+lr2 = [lr2(1) / lr2(3), lr2(2)/lr2(3), 1];
+lr3 = [lr3(1) / lr3(3), lr3(2)/lr3(3), 1];
+lr4 = [lr4(1) / lr4(3), lr4(2)/lr4(3), 1];
+
 lm1 = [lr1(1)*lr3(1), lr1(1)*lr3(2) + lr1(2)*lr3(1), lr1(2)*lr3(2)];
 lm2 = [lr2(1)*lr4(1), lr2(1)*lr4(2) + lr2(2)*lr4(1), lr2(2)*lr4(2)];
 
 A = [lm1(1:2); lm2(1:2)];
-b = [lm1(3); lm2(3)];
+b = -[lm1(3); lm2(3)];
 sol = linsolve(A, b);
 
 sv = [sol' 1];
@@ -229,44 +212,28 @@ sv = [sol' 1];
 S = [sv(1) sv(2); sv(2) sv(3)];
 R = chol(S);
 
-Ha = inv([R [0;0]; 0 0 1]);
+Ha = [inv(R) [0;0]; 0 0 1];
 
-H3 = Ha*Hp';
+H3 = Ha;
 
-close all
 I3 = apply_H(I, H3);
+
+%tform = projective2d(H3');
+%I3 = imwarp(I, tform);
+
 figure; imshow(uint8(I3));
 hold on;
 
-pr1 = H3*pr1';
-pr1 = [pr1(1)/ pr1(3), pr1(2)/pr1(3), 1];
-pr2 = H3*pr2';
-pr2 = [pr2(1)/ pr2(3), pr2(2)/pr2(3), 1];
-pr3 = H3*pr3';
-pr3 = [pr3(1)/ pr3(3), pr3(2)/pr3(3), 1];
-pr4 = H3*pr4';
-pr4 = [pr4(1)/ pr4(3), pr4(2)/pr4(3), 1];
-pr5 = H3*pr5';
-pr5 = [pr5(1)/ pr5(3), pr5(2)/pr5(3), 1];
-pr6 = H3*pr6';
-pr6 = [pr6(1)/ pr6(3), pr6(2)/pr6(3), 1];
-pr7 = H3*pr7';
-pr7 = [pr7(1)/ pr7(3), pr7(2)/pr7(3), 1];
-pr8 = H3*pr8';
-pr8 = [pr8(1)/ pr8(3), pr8(2)/pr8(3), 1];
-
 % Now we compute the lines from the transformed points
-coefficients = polyfit([pr1(1), pr2(1)], [pr1(2), pr2(2)], 1);
-lr1 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
+lr1 = inv(H3)' * l1';
+lr2 = inv(H3)' * l2';
+lr3 = inv(H3)' * l3';
+lr4 = inv(H3)' * l4';
 
-coefficients = polyfit([pr3(1), pr4(1)], [pr3(2), pr4(2)], 1);
-lr2 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
-
-coefficients = polyfit([pr5(1), pr6(1)], [pr5(2), pr6(2)], 1);
-lr3 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
-
-coefficients = polyfit([pr7(1), pr8(1)], [pr7(2), pr8(2)], 1);
-lr4 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
+lr1 = [lr1(1) / lr1(3), lr1(2)/lr1(3), 1];
+lr2 = [lr2(1) / lr2(3), lr2(2)/lr2(3), 1];
+lr3 = [lr3(1) / lr3(3), lr3(2)/lr3(3), 1];
+lr4 = [lr4(1) / lr4(3), lr4(2)/lr4(3), 1];
 
 t=1:0.1:1000;
 plot(t, -(lr1(1)*t + lr1(3)) / lr1(2), 'y');
@@ -277,16 +244,30 @@ plot(t, -(lr4(1)*t + lr4(3)) / lr4(2), 'y');
 cos1 = dot(l1(1:2), l2(1:2)) / (norm(l1(1:2)) * norm(l2(1:2)));
 cosr1 = dot(lr1(1:2), lr2(1:2)) / (norm(lr1(1:2)) * norm(lr2(1:2)));
 angle1 = acos(cos1);
-disp("Angle 1:");disp(angle1);
+disp("Angle parallel 1:");disp(rad2deg(angle1));
 angler1 = acos(cosr1);
-disp("Angle 1 rectified:");disp(angler1);
+disp("Angle parallel 1 rectified:");disp(rad2deg(angler1));
 
 cos2 = dot(l3(1:2), l4(1:2)) / (norm(l3(1:2)) * norm(l4(1:2)));
 angle2 = acos(cos2);
-disp("Angle 2:");disp(angle2);
+disp("Angle paralle l2:");disp(rad2deg(angle2));
 cosr2 = dot(lr3(1:2), lr4(1:2)) / (norm(lr3(1:2)) * norm(lr4(1:2)));
 angler2 = acos(cosr2);
-disp("Angle 2 rectified:");disp(angler2);
+disp("Angle parallel 2 rectified:");disp(rad2deg(angler2));
+
+cos1 = dot(l1(1:2), l3(1:2)) / (norm(l1(1:2)) * norm(l3(1:2)));
+cosr1 = dot(lr1(1:2), lr3(1:2)) / (norm(lr1(1:2)) * norm(lr3(1:2)));
+angle1 = acos(cos1);
+disp("Angle ortho 1:");disp(rad2deg(angle1));
+angler1 = acos(cosr1);
+disp("Angle ortho 1 rectified:");disp(rad2deg(angler1));
+
+cos2 = dot(l2(1:2), l4(1:2)) / (norm(l2(1:2)) * norm(l4(1:2)));
+angle2 = acos(cos2);
+disp("Angle ortho l2:");disp(rad2deg(angle2));
+cosr2 = dot(lr2(1:2), lr4(1:2)) / (norm(lr2(1:2)) * norm(lr4(1:2)));
+angler2 = acos(cosr2);
+disp("Angle ortho 2 rectified:");disp(rad2deg(angler2));
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -296,8 +277,6 @@ disp("Angle 2 rectified:");disp(angler2);
 %       the stratified method (affine + metric). 
 %       Crop the initial image so that only the left facade is visible.
 %       Show the (properly) transformed lines that use in every step.
-
-close all
 
 
 % choose the image points
@@ -340,6 +319,7 @@ plot(t, -(l1(1)*t + l1(3)) / l1(2), 'y');
 plot(t, -(l2(1)*t + l2(3)) / l2(2), 'y');
 plot(t, -(l3(1)*t + l3(3)) / l3(2), 'y');
 plot(t, -(l4(1)*t + l4(3)) / l4(2), 'y');
+hold off;
 
 % ToDo: compute the homography that affinely rectifies the image
 
@@ -359,21 +339,21 @@ figure; imshow(uint8(I2));
 
 % ToDo: compute the transformed lines lr1, lr2, lr3, lr4
 
-pr1 = H*p1;
+pr1 = H'*p1;
 pr1 = [pr1(1)/ pr1(3), pr1(2)/pr1(3), 1];
-pr2 = H*p2;
+pr2 = H'*p2;
 pr2 = [pr2(1)/ pr2(3), pr2(2)/pr2(3), 1];
-pr3 = H*p3;
+pr3 = H'*p3;
 pr3 = [pr3(1)/ pr3(3), pr3(2)/pr3(3), 1];
-pr4 = H*p4;
+pr4 = H'*p4;
 pr4 = [pr4(1)/ pr4(3), pr4(2)/pr4(3), 1];
-pr5 = H*p5;
+pr5 = H'*p5;
 pr5 = [pr5(1)/ pr5(3), pr5(2)/pr5(3), 1];
-pr6 = H*p6;
+pr6 = H'*p6;
 pr6 = [pr6(1)/ pr6(3), pr6(2)/pr6(3), 1];
-pr7 = H*p7;
+pr7 = H'*p7;
 pr7 = [pr7(1)/ pr7(3), pr7(2)/pr7(3), 1];
-pr8 = H*p8;
+pr8 = H'*p8;
 pr8 = [pr8(1)/ pr8(3), pr8(2)/pr8(3), 1];
 
 coefficients = polyfit([pr1(1), pr2(1)], [pr1(2), pr2(2)], 1);
@@ -396,6 +376,7 @@ plot(t, -(lr1(1)*t + lr1(3)) / lr1(2), 'y');
 plot(t, -(lr2(1)*t + lr2(3)) / lr2(2), 'y');
 plot(t, -(lr3(1)*t + lr3(3)) / lr3(2), 'y');
 plot(t, -(lr4(1)*t + lr4(3)) / lr4(2), 'y');
+hold off;
 
 % ToDo: to evaluate the results, compute the angle between the different pair 
 % of lines before and after the image transformation
