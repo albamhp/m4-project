2c11e032ffc90fcd546dc6927e191cff5c8e0d7b
albamhp <albaherrerapalacio@gmail.com>
2018-12-17 16:56:28 +0100

    Trying lines

--- lab1/lab1.m
+++ lab1/lab1.m
@@ -231,9 +231,63 @@ R = chol(S);
 
 Ha = inv([R [0;0]; 0 0 1]);
 
+H3 = Ha*Hp';
+
 close all
-I3 = apply_H(I, Ha*Hp');
+I3 = apply_H(I, H3);
 figure; imshow(uint8(I3));
+hold on;
+
+pr1 = H3*pr1';
+pr1 = [pr1(1)/ pr1(3), pr1(2)/pr1(3), 1];
+pr2 = H3*pr2';
+pr2 = [pr2(1)/ pr2(3), pr2(2)/pr2(3), 1];
+pr3 = H3*pr3';
+pr3 = [pr3(1)/ pr3(3), pr3(2)/pr3(3), 1];
+pr4 = H3*pr4';
+pr4 = [pr4(1)/ pr4(3), pr4(2)/pr4(3), 1];
+pr5 = H3*pr5';
+pr5 = [pr5(1)/ pr5(3), pr5(2)/pr5(3), 1];
+pr6 = H3*pr6';
+pr6 = [pr6(1)/ pr6(3), pr6(2)/pr6(3), 1];
+pr7 = H3*pr7';
+pr7 = [pr7(1)/ pr7(3), pr7(2)/pr7(3), 1];
+pr8 = H3*pr8';
+pr8 = [pr8(1)/ pr8(3), pr8(2)/pr8(3), 1];
+
+% Now we compute the lines from the transformed points
+coefficients = polyfit([pr1(1), pr2(1)], [pr1(2), pr2(2)], 1);
+lr1 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
+
+coefficients = polyfit([pr3(1), pr4(1)], [pr3(2), pr4(2)], 1);
+lr2 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
+
+coefficients = polyfit([pr5(1), pr6(1)], [pr5(2), pr6(2)], 1);
+lr3 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
+
+coefficients = polyfit([pr7(1), pr8(1)], [pr7(2), pr8(2)], 1);
+lr4 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
+
+t=1:0.1:1000;
+plot(t, -(lr1(1)*t + lr1(3)) / lr1(2), 'y');
+plot(t, -(lr2(1)*t + lr2(3)) / lr2(2), 'y');
+plot(t, -(lr3(1)*t + lr3(3)) / lr3(2), 'y');
+plot(t, -(lr4(1)*t + lr4(3)) / lr4(2), 'y');
+
+cos1 = dot(l1(1:2), l2(1:2)) / (norm(l1(1:2)) * norm(l2(1:2)));
+cosr1 = dot(lr1(1:2), lr2(1:2)) / (norm(lr1(1:2)) * norm(lr2(1:2)));
+angle1 = acos(cos1);
+disp("Angle 1:");disp(angle1);
+angler1 = acos(cosr1);
+disp("Angle 1 rectified:");disp(angler1);
+
+cos2 = dot(l3(1:2), l4(1:2)) / (norm(l3(1:2)) * norm(l4(1:2)));
+angle2 = acos(cos2);
+disp("Angle 2:");disp(angle2);
+cosr2 = dot(lr3(1:2), lr4(1:2)) / (norm(lr3(1:2)) * norm(lr4(1:2)));
+angler2 = acos(cosr2);
+disp("Angle 2 rectified:");disp(angler2);
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% 4. Affine and Metric Rectification of the left facade of image 0001
@@ -244,8 +298,7 @@ figure; imshow(uint8(I3));
 %       Show the (properly) transformed lines that use in every step.
 
 close all
-clear all
-clc
+
 
 % choose the image points
 I=imread('Data/0001_s.png');
@@ -366,66 +419,66 @@ disp("Angle 2 rectified:");disp(angler2);
 % indices of lines
 
 % choose the image points
-I=imread('Data/0000_s.png');
-A = load('Data/0000_s_info_lines.txt');
-
-% indices of lines
-i = 424;
-p1 = [A(i,1) A(i,2) 1]';
-p2 = [A(i,3) A(i,4) 1]';
-i = 240;
-p3 = [A(i,1) A(i,2) 1]';
-p4 = [A(i,3) A(i,4) 1]';
-i = 712;
-p5 = [A(i,1) A(i,2) 1]';
-p6 = [A(i,3) A(i,4) 1]';
-i = 565;
-p7 = [A(i,1) A(i,2) 1]';
-p8 = [A(i,3) A(i,4) 1]';
-
-i = 227;
-p9 = [A(i,1) A(i,2) 1]';
-p10 = [A(i,3) A(i,4) 1]';
-i = 367;
-p11 = [A(i,1) A(i,2) 1]';
-p12 = [A(i,3) A(i,4) 1]';
-
-
-% ToDo: compute the lines l1, l2, l3, l4, that pass through the different pairs of points
-
-coefficients = polyfit([p1(1), p2(1)], [p1(2), p2(2)], 1);
-l1 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
-coefficients = polyfit([p3(1), p4(1)], [p3(2), p4(2)], 1);
-l2 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
-coefficients = polyfit([p5(1), p6(1)], [p5(2), p6(2)], 1);
-l3 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
-coefficients = polyfit([p7(1), p8(1)], [p7(2), p8(2)], 1);
-l4 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
-coefficients = polyfit([p9(1), p10(1)], [p9(2), p10(2)], 1);
-l5 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
-coefficients = polyfit([p11(1), p12(1)], [p11(2), p12(2)], 1);
-l6 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
-
-lm1 = [l1(1)*l3(1), l1(1)*l3(2) + l1(2)*l3(1), l1(2)*l3(2)];
-lm2 = [l2(1)*l4(1), l2(1)*l4(2) + l2(2)*l4(1), l2(2)*l4(2)];
-lm3 = [l1(1)*l4(1), l1(1)*l4(2) + l1(2)*l4(1), l1(2)*l4(2)];
-lm4 = [l2(1)*l3(1), l2(1)*l3(2) + l2(2)*l3(1), l2(2)*l3(2)];
-lm5 = [l5(1)*l6(1), l5(1)*l6(2) + l5(2)*l6(1), l5(2)*l6(2)];
-
-
-A = [lm1(1:2); lm2(1:2); lm3(1:2); lm4(1:2); lm5(1:2)];
-b = [lm1(3); lm2(3); lm3(3); lm4(3); lm5(3)];
-
-sol = linsolve(A, b);
-
-sv = [sol' 1];
-
-S = [sv(1) sv(2); sv(2) sv(3)];
-R = chol(S);
-
-Ha = inv([R [0;0]; 0 0 1]);
-
-close all
-I3 = apply_H(I, Ha*Hp');
-figure; imshow(uint8(I3));
+% I=imread('Data/0000_s.png');
+% A = load('Data/0000_s_info_lines.txt');
+% 
+% % indices of lines
+% i = 424;
+% p1 = [A(i,1) A(i,2) 1]';
+% p2 = [A(i,3) A(i,4) 1]';
+% i = 240;
+% p3 = [A(i,1) A(i,2) 1]';
+% p4 = [A(i,3) A(i,4) 1]';
+% i = 712;
+% p5 = [A(i,1) A(i,2) 1]';
+% p6 = [A(i,3) A(i,4) 1]';
+% i = 565;
+% p7 = [A(i,1) A(i,2) 1]';
+% p8 = [A(i,3) A(i,4) 1]';
+% 
+% i = 227;
+% p9 = [A(i,1) A(i,2) 1]';
+% p10 = [A(i,3) A(i,4) 1]';
+% i = 367;
+% p11 = [A(i,1) A(i,2) 1]';
+% p12 = [A(i,3) A(i,4) 1]';
+% 
+% 
+% % ToDo: compute the lines l1, l2, l3, l4, that pass through the different pairs of points
+% 
+% coefficients = polyfit([p1(1), p2(1)], [p1(2), p2(2)], 1);
+% l1 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
+% coefficients = polyfit([p3(1), p4(1)], [p3(2), p4(2)], 1);
+% l2 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
+% coefficients = polyfit([p5(1), p6(1)], [p5(2), p6(2)], 1);
+% l3 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
+% coefficients = polyfit([p7(1), p8(1)], [p7(2), p8(2)], 1);
+% l4 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
+% coefficients = polyfit([p9(1), p10(1)], [p9(2), p10(2)], 1);
+% l5 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
+% coefficients = polyfit([p11(1), p12(1)], [p11(2), p12(2)], 1);
+% l6 = [coefficients(1)/coefficients(2) -1/coefficients(2) 1];
+% 
+% lm1 = [l1(1)*l3(1),(l1(1)*l3(2)+l1(2)*l3(1))/2, l1(2)*l3(2), (l1(1)*l3(3)+l1(3)*l3(1))/2, (l1(2)*l3(3)+l1(3)*l3(2))/2, l1(3)*l3(3)];
+% lm2 = [l2(1)*l4(1),(l2(1)*l4(2)+l2(2)*l4(1))/2, l2(2)*l4(2), (l2(1)*l4(3)+l2(3)*l4(1))/2, (l2(2)*l4(3)+l2(3)*l4(2))/2, l2(3)*l4(3)];
+% lm3 = [l1(1)*l4(1),(l1(1)*l4(2)+l1(2)*l4(1))/2, l1(2)*l4(2), (l1(1)*l4(3)+l1(3)*l4(1))/2, (l1(2)*l4(3)+l1(3)*l4(2))/2, l1(3)*l4(3)];
+% lm4 = [l2(1)*l3(1),(l2(1)*l3(2)+l2(2)*l3(1))/2, l2(2)*l3(2), (l2(1)*l3(3)+l2(3)*l3(1))/2, (l2(2)*l3(3)+l2(3)*l3(2))/2, l2(3)*l3(3)];
+% lm5 = [l5(1)*l6(1),(l5(1)*l6(2)+l5(2)*l6(1))/2, l5(2)*l6(2), (l5(1)*l6(3)+l5(3)*l6(1))/2, (l5(2)*l6(3)+l5(3)*l6(2))/2, l5(3)*l6(3)];
+% 
+% 
+% A = [lm1(1:2); lm2(1:2); lm3(1:2); lm4(1:2); lm5(1:2)];
+% b = [lm1(3); lm2(3); lm3(3); lm4(3); lm5(3)];
+% 
+% sol = linsolve(A, b);
+% 
+% sv = [sol' 1];
+% 
+% S = [sv(1) sv(2); sv(2) sv(3)];
+% K = chol(S);
+% 
+% H = inv([K*K' K*K'; K*K' K*K']);
+% 
+% close all
+% I5 = apply_H(I, H);
+% figure; imshow(uint8(I5));
 
