578724374d072aae83e12fbdb3ada4f23474af55
Jorge López <jorge.lopez@neosentec.com>
2019-01-10 16:30:07 +0100

    Minor changes

---
+++ lab3/fundamental_matrix.m
@@ -0,0 +1,6 @@
+function [F] = fundamental_matrix(x1, x2)
+%FUNDAMENTAL_MATRIX Calculate the fundamental matrix using the normalised 8 point algorithm
+%   x1 Set of points of the first image
+%   x2 Set of points of the second image
+    % ToDo
+end
