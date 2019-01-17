af9f35c75222bafe19d4b36d9e829c5f48a6a5c1
albamhp <albaherrerapalacio@gmail.com>
2019-01-17 17:44:42 +0100

    Triangulation done

--- lab3/triangulate.m
+++ lab3/triangulate.m
@@ -1,4 +1,4 @@
-function [pts3D] = triangulate(P0, P1, pts0, pts1, imgsizes)
+function [pts3D] = triangulate(x1, x2, P1, P2, imsize)
 
 %   Function triangulate.m that performs a triangulation
 %   with the homogeneous algebraic method (DLT)
@@ -9,5 +9,28 @@ function [pts3D] = triangulate(P0, P1, pts0, pts1, imgsizes)
 %           - P1 and P2 are the two camera matrices
 %           - imsize is a two-dimensional vector with the image size
 
+    npts=size(x1, 2);
+    pts3D=zeros(3, npts);
+
+    H1=[2/imsize(1, 1) 0 -1
+        0 2/imsize(2, 1) -1
+        0 0 1];
+    H2=[2/imsize(1, 2) 0 -1
+        0 2/imsize(2, 2) -1
+        0 0 1];
+     
+    % left multiply Ps and points
+    nP1=H1*P1;
+    nP2=H2*P2;
+    nx1=H1(1:2, 1:2)*x1 + repmat(H1(1:2, 3), 1, npts);
+    nx2=H2(1:2, 1:2)*x2 + repmat(H2(1:2, 3), 1, npts);
+    
+    % at this point, normalization has been performed with the H's above
+    for i=1:npts
+        A=[nx1(:,i)*nP1(3,:)-nP1(1:2,:); nx2(:,i)*nP2(3,:)-nP2(1:2,:)];
+        % Linear-Eigen
+        [~, ~, V]=svd(A, 0);
+        pts3D(:,i)=V(1:3, 4)./V(4, 4);
+    end
 
 end
