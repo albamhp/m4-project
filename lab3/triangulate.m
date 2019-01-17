93da006ca7f5553c518f1acd805ec12a319c209e
albamhp <albaherrerapalacio@gmail.com>
2019-01-17 17:20:28 +0100

    Triangulation function

---
+++ lab3/triangulate.m
@@ -0,0 +1,13 @@
+function [pts3D] = triangulate(P0, P1, pts0, pts1, imgsizes)
+
+%   Function triangulate.m that performs a triangulation
+%   with the homogeneous algebraic method (DLT)
+%
+%       The entries are (x1, x2, P1, P2, imsize), where:
+%           - x1, and x2 are the Euclidean coordinates of two matching 
+%             points in two different images.
+%           - P1 and P2 are the two camera matrices
+%           - imsize is a two-dimensional vector with the image size
+
+
+end
